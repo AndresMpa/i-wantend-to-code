a3b464fcfcff7979c1944c8bfec8ae183dcbdb25
AndresMpa <andresprieto2703@gmail.com>
2021-12-19 02:37:15 -0500

    Adding animations and colors

--- src/main.js
+++ src/main.js
@@ -5,7 +5,3 @@ import { loadData } from "./util/loadData.js";
   loadData();
   eventHandler();
 })();
-
-window.addEventListener("click", (e) => {
-  console.log(e.target);
-});
--- src/util/pickHandler.js
+++ src/util/pickHandler.js
@@ -1,7 +1,14 @@
 export const pickHandler = (pick) => {
+  const picking = document.querySelector("#picking");
   const board = document.querySelector("#board");
 
-  board.classList === ""
-    ? board.classList = "hideBoard"
-    : (board.style.display = "none");
+  const system = document.querySelector("#system");
+  const user = document.querySelector("#user");
+
+  board.style.display = "none";
+
+  picking.classList.add("animate__animated", "animate__jackInTheBox");
+  picking.style.display = "flex";
+  console.log(picking);
+  console.log(pick);
 };
