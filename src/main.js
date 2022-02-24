112c02049e8f8082a5277ace2e85d8ae380c3fba
Andres M. Prieto <andresprieto2703@gmail.com>
2022-02-24 13:33:18 -0500

    Adding a serviceWorker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { eventHandler } from "./util/eventHandler.js";
 import { resetBoard } from "./util/resetBoard.js";
-import { displayRules } from './util/extras.js';
+import { displayRules } from "./util/extras.js";
 import { loadData } from "./util/loadData.js";
 
 (() => {
@@ -8,4 +8,10 @@ import { loadData } from "./util/loadData.js";
   eventHandler();
   resetBoard();
   displayRules();
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/serviceWorker.js").catch((error) => {
+      console.error(error.message);
+    });
+  }
 })();
